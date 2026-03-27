6d474e2aa8a422f7b1dd8a981b091e957d454439
innovacode3 <innovacode3@gmail.com>
2026-03-27 18:23:07 -0500

    52 cambio

--- routes/poomsaeInscripcionRoutes.js
+++ routes/poomsaeInscripcionRoutes.js
@@ -486,7 +486,7 @@ router.get('/evento/poomsae/obtenerCategoriaCinturon/:id_evento_fk/:modalidad', 
                         categoria_suscrito_alumno_poomsae AS nombre_categoria, 
                         cinturon_suscrito_alumno_poomsae AS cinturones
                  FROM suscrito_alumno_poomsae
-                 WHERE id_evento_fk = $1 && modalidad = $2
+                 WHERE id_evento_fk = $1 AND modalidad = $2
                  GROUP BY categoria_suscrito_alumno_poomsae, cinturon_suscrito_alumno_poomsae
                  ORDER BY
                     CASE
