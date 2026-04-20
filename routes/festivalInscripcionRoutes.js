89fdd917dbbc65552e8e20e2bfa90765b4ac1510
innovacode3 <innovacode3@gmail.com>
2026-04-20 02:54:46 -0500

    73 cambio

--- routes/festivalInscripcionRoutes.js
+++ routes/festivalInscripcionRoutes.js
@@ -321,7 +321,7 @@ router.get('/evento/lista_alumnos_festival/:id_evento_fk', async (req, res) => {
 })
 
 //Obtener la lista de alumnos inscritos por delegacion para admin, public, loguedo (id_evento, id_delegacion)
-router.get('evento/lista_alumnos_festival_delegacion/:id_evento_fk/:id_delegacion_fk', async (req, res) => {
+router.get('/evento/lista_alumnos_festival_delegacion/:id_evento_fk/:id_delegacion_fk', async (req, res) => {
     try {
         const { id_evento_fk, id_delegacion_fk } = req.params;
 
@@ -339,7 +339,7 @@ router.get('evento/lista_alumnos_festival_delegacion/:id_evento_fk/:id_delegacio
         }
 
         res.json(data);
-        
+
     } catch (err) {
         console.error('Error servidor:', err);
 
